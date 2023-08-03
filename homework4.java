a36f52c88d5bf0d8d2bcb84c54b41a652aebe517
Dioneer <Gulkina_em@mail.ru>
2023-08-03 19:36:06 +0300

    lesson4

--- homework4.java
+++ homework4.java
@@ -1,95 +1,95 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
+// import java.util.ArrayList;
+// import java.util.LinkedList;
 
-public class homework4 {
-	static List<Integer> result = new ArrayList<Integer>();
+// public class homework4 {
+// static List<Integer> result = new ArrayList<Integer>();
 
-	public static void main(String[] args) {
-		LinkedList<Object> ll = new LinkedList<>();
+// public static void main(String[] args) {
+// LinkedList<Object> ll = new LinkedList<>();
 
-		if (args.length == 0 || args.length != 4) {
-			// При отправке кода на Выполнение, вы можете варьировать эти параметры
-			ll.add(1);
-			ll.add("One");
-			ll.add(2);
-			ll.add("Two");
-		} else {
-			ll.add(Integer.parseInt(args[0]));
-			ll.add(args[1]);
-			ll.add(Integer.parseInt(args[2]));
-			ll.add(args[3]);
-		}
+// if (args.length == 0 || args.length != 4) {
+// // При отправке кода на Выполнение, вы можете варьировать эти параметры
+// ll.add(1);
+// ll.add("One");
+// ll.add(2);
+// ll.add("Two");
+// } else {
+// ll.add(Integer.parseInt(args[0]));
+// ll.add(args[1]);
+// ll.add(Integer.parseInt(args[2]));
+// ll.add(args[3]);
+// }
 
-		LLTasks answer = new LLTasks();
-		System.out.println(ll);
-		LinkedList<Object> reversedList = answer.revert(ll);
-		System.out.println(reversedList);
-	}
+// LLTasks answer = new LLTasks();
+// System.out.println(ll);
+// LinkedList<Object> reversedList = answer.revert(ll);
+// System.out.println(reversedList);
+// }
 
-}
+// }
 
-class LLTasks {
-	public static LinkedList<Object> revert(LinkedList<Object> ll) {
-		// Напишите свое решение ниже
-		LinkedList<Object> newList = new LinkedList<>();
-		for (int i = ll.size() - 1; i >= 0; i--) {
-			newList.add(ll.get(i));
-		}
-		return newList;
-	}
-}
+// class LLTasks {
+// public static LinkedList<Object> revert(LinkedList<Object> ll) {
+// // Напишите свое решение ниже
+// LinkedList<Object> newList = new LinkedList<>();
+// for (int i = ll.size() - 1; i >= 0; i--) {
+// newList.add(ll.get(i));
+// }
+// return newList;
+// }
+// }
 
-class MyQueue<T> {
-	// Напишите свое решение ниже
-	LinkedList<T> queue = new LinkedList<T>();
+// class MyQueue<T> {
+// // Напишите свое решение ниже
+// LinkedList<T> queue = new LinkedList<T>();
 
-	public void enqueue(T element) {
-		// Напишите свое решение ниже
-		queue.addLast(element);
-	}
+// public void enqueue(T element) {
+// // Напишите свое решение ниже
+// queue.addLast(element);
+// }
 
-	public T dequeue() {
-		// Напишите свое решение ниже
-		return queue.pollFirst();
-	}
+// public T dequeue() {
+// // Напишите свое решение ниже
+// return queue.pollFirst();
+// }
 
-	public T first() {
-		// Напишите свое решение ниже
-		return queue.peekFirst();
-	}
+// public T first() {
+// // Напишите свое решение ниже
+// return queue.peekFirst();
+// }
 
-	public LinkedList<T> getElements() {
-		// Напишите свое решение ниже
-		return queue;
-	}
-}
+// public LinkedList<T> getElements() {
+// // Напишите свое решение ниже
+// return queue;
+// }
+// }
 
-class Calculator {
-	static ArrayDeque<Integer> result = new ArrayDeque<Integer>();
+// class Calculator {
+// static ArrayDeque<Integer> result = new ArrayDeque<Integer>();
 
-	public int calculate(char op, int a, int b) {
-		int returnres;
-		// Напишите свое решение ниже
-		switch (op) {
-			case '+':
-				result.add(a + b);
-				break;
-			case '-':
-				result.add(a - b);
-				break;
-			case '/':
-				result.add(a / b);
-				break;
-			case '*':
-				result.add(a * b);
-				break;
-			case '<':
-				int last = result.removeLast();
-				result.add(result.getLast());
-				break;
-			default:
-				break;
-		}
-		return result.getLast();
-	}
-}
+// public int calculate(char op, int a, int b) {
+// int returnres;
+// // Напишите свое решение ниже
+// switch (op) {
+// case '+':
+// result.add(a + b);
+// break;
+// case '-':
+// result.add(a - b);
+// break;
+// case '/':
+// result.add(a / b);
+// break;
+// case '*':
+// result.add(a * b);
+// break;
+// case '<':
+// int last = result.removeLast();
+// result.add(result.getLast());
+// break;
+// default:
+// break;
+// }
+// return result.getLast();
+// }
+// }
--- lesson4.java
+++ lesson4.java
@@ -1,5 +1,110 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class lesson4 {
 	public static void main(String[] args) {
-
+		// task1();
+		// task2();
+		// task3();
+		task4();
 	}
+
+	// static void task1() {
+	// List<Integer> list1 = new ArrayList<Integer>();
+	// List<Integer> list2 = new LinkedList<Integer>();
+
+	// Long start = System.currentTimeMillis();
+
+	// for (int i = 0; i < 100000; i++) {
+	// list1.add(0, i);
+	// }
+
+	// Long middle = System.currentTimeMillis();
+
+	// for (int i = 0; i < 100000; i++) {
+	// list2.add(0, i);
+	// }
+	// Long end = System.currentTimeMillis();
+
+	// System.out.println("First sycle: " + (middle - start));
+	// System.out.println("First sycle: " + (end - middle));
+	// }
+
+	// static void task2() {
+	// LinkedList<String> list = new LinkedList<>();
+	// Scanner scanner = new Scanner(System.in);
+	// boolean flag = true;
+	// while (flag) {
+	// System.out.println("Enter command: ");
+	// String line = scanner.nextLine();
+	// String[] arr = line.split("~");
+	// int num = Integer.parseInt(arr[1]);
+	// String text = arr[0];
+	// System.out.println();
+	// switch (text) {
+	// case "print":
+	// System.out.println(list.remove(num));
+	// break;
+	// case "exit":
+	// System.out.println("Goodbye");
+	// flag = false;
+	// break;
+	// default:
+	// list.add(num, text);
+	// break;
+	// }
+	// }
+	// }
+
+	// static void task3() {
+	// LinkedList<String> list = new LinkedList<>();
+	// Scanner scanner = new Scanner(System.in);
+	// boolean flag = true;
+	// while (flag) {
+	// System.out.println("Enter command: ");
+	// String text = scanner.nextLine();
+	// switch (text) {
+	// case "print":
+	// ListIterator listIterator = list.listIterator(list.size());
+	// while (listIterator.hasPrevious()) {
+	// System.out.println(listIterator.previous());
+	// }
+	// break;
+	// case "exit":
+	// System.out.println("Goodbye");
+	// flag = false;
+	// break;
+	// case "revert":
+	// System.out.println(list.removeLast());
+	// break;
+	// default:
+	// System.out.println(list.add(text));
+	// break;
+	// }
+	// }
+	// }
+
+	static void task4() {
+		Integer[] arr = { 1, 2, 3, 4, 5, 6 };
+		Stack<Integer> stack = new Stack<>();
+		Queue<Integer> queue = new LinkedList<>();
+		for (int i = 0; i < arr.length; i++) {
+			stack.push(arr[i]);
+			queue.offer(arr[i]);
+		}
+		while (!stack.isEmpty()) {
+			System.out.println(stack.pop());
+		}
+
+		while (!queue.isEmpty()) {
+			System.out.println(queue.poll());
+		}
+	}
+
 }
